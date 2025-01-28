dc3969be163cf95eb35dc91482c0cdc5e550a88e
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-01-28 21:23:49 +0000

    did alot

--- app.rb
+++ app.rb
@@ -4,3 +4,45 @@ require "sinatra/reloader"
 get("/") do
   erb(:rules)
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:scissors)
+end
